2733f532947fcb3fa6131e5c04a58c4b1ad0f2d3
Oswaldo Zavala <oszavala@gmail.com>
2019-08-08 17:30:59 -0500

    Create a cart and pass test

---
+++ test/models/cart_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class CartTest < MiniTest::Test
+  def test_add_one_item
+    cart= Cart.new
+    cart.add_item 1
+    assert_equal cart.empty?, false
+  end
+
+  def test_adds_up_1n_quantity
+    cart = Cart.new
+    3.times { cart.add_item 1 }
+
+    assert_equal 1, cart.items.length
+    assert_equal 3, cart.items.first.quantity
+  end
+
+  def test_retrieves_products
+    product = Product.create! name: 'tomato', price: 1
+
+    cart = Cart.new
+    cart.add_item product.id
+
+    assert_kind_of Product, cart.items.first.product
+  end
+end
