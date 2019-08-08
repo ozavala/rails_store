c0e41ab15bc7cde1e081042ba7993674cfb03afc
Oswaldo Zavala <oszavala@gmail.com>
2019-08-08 18:06:08 -0500

    Changed Sqlitedb for Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_08_144204) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
